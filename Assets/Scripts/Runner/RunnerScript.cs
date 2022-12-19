040353befcffcbc2feffe8846aaf82f80760b440
Ege Güvenir <egeguvenirdev@gmail.com>
2022-12-19 22:42:39 +0300

    player control && stickman pack

--- Assets/Scripts/Runner/RunnerScript.cs
+++ Assets/Scripts/Runner/RunnerScript.cs
@@ -9,17 +9,9 @@ public class RunnerScript : MonoBehaviour
     [Header("Scripts and Transforms")]
     [SerializeField] private Transform model;
     [SerializeField] private Transform localMoverTarget;
-    [SerializeField] private PathCreator pathCreator;
     [SerializeField] private SimpleAnimancer animancer;
     [SerializeField] private PlayerSwerve playerSwerve;
-    [SerializeField] private PathManager pathManager;
-
-    [Header("Path Settings")]
-    [SerializeField] private float distance = 0;
-    [SerializeField] private float startDistance = 0;
-    [SerializeField] private float clampLocalX = 2f;
     
-
     [Header("Run Settings")]
     [SerializeField] private float runSpeed = 2;
     [SerializeField] private float localTargetswipeSpeed = 2f;
@@ -27,33 +19,24 @@ public class RunnerScript : MonoBehaviour
     [SerializeField] private float swipeRotateLerpSpeed = 2f;
 
     private Vector3 oldPosition;
-    private bool canRun = false;
     private bool canSwerve = false;
     private bool canFollow = true;
-    private bool moveEnabled = false;
     private string currentAnimName = "Walking";
 
     void Awake()
     {
-        playerSwerve.OnSwerve += PlayerSwipe_OnSwerve;
-        distance = startDistance;
+        playerSwerve.OnSwerveStart += PlayerSwipe_OnPointerDown;
+        playerSwerve.OnSwerveEnd += PlayerSwipe_OnPointerUp;
         oldPosition = localMoverTarget.localPosition;
     }
 
     public void Init()
     {
-        pathCreator = FindObjectOfType<PathCreator>();
-    }
-
-    private void Start()
-    {
         PlayAnimation("StartIdle");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        UpdatePath();
         FollowLocalMoverTarget();
         oldPosition = model.localPosition;
     }
@@ -62,54 +45,23 @@ public class RunnerScript : MonoBehaviour
     {
         if (checkRun)
         {
-            canRun = true;
             canSwerve = true;
             canFollow = true;
         }
         else
         {
-            canRun = false;
             canSwerve = false;
         }
     }
 
-    void UpdatePath()
+    private void PlayerSwipe_OnPointerDown()
     {
-        if (canRun)
-        {
-            distance += runSpeed * Time.deltaTime;
-            transform.position = pathCreator.path.GetPointAtDistance(distance);
-            transform.eulerAngles = pathCreator.path.GetRotationAtDistance(distance).eulerAngles + new Vector3(0f, 0f, 90f);
-        }
+        PlayAnimation("Running");
     }
 
-    private void PlayerSwipe_OnSwerve(Vector2 direction)
+    private void PlayerSwipe_OnPointerUp()
     {
-        /*if (canClean)
-        {
-            canFollow = false;
-            model.localPosition = model.localPosition + Vector3.right * direction.x * localModelSwipeSpeed * Time.deltaTime;
-            model.localPosition = model.localPosition + Vector3.forward * direction.y * localModelSwipeSpeed * Time.deltaTime;
-
-            Vector3 pos = model.position;
-            pos.x = Mathf.Clamp(pos.x, -clampModelLocalX, clampModelLocalX);
-            pos.z = Mathf.Clamp(pos.z, (transform.position.z + -clampModelLocalZ), (transform.position.z + clampModelLocalZ));
-            model.position = pos;
-        }*/
-
-        if (canSwerve)
-        {
-            localMoverTarget.localPosition = localMoverTarget.localPosition + Vector3.right * direction.x * localTargetswipeSpeed * Time.deltaTime;
-            ClampLocalPosition();
-        }
-    }
-
-    void ClampLocalPosition()
-    {
-        Vector3 pos = localMoverTarget.localPosition;
-        pos.x = Mathf.Clamp(pos.x, -clampLocalX, clampLocalX);
-        localMoverTarget.localPosition = pos;
-
+        PlayAnimation("Idle");
     }
 
     void FollowLocalMoverTarget()
@@ -139,48 +91,14 @@ public class RunnerScript : MonoBehaviour
         currentAnimName = animName;
     }
 
-    public void DodgeBack()
-    {
-        StartCoroutine(DodgeBackProcess());
-    }
-
-    IEnumerator DodgeBackProcess()
-    {
-        canSwerve = false;
-        canRun = false;
-        animancer.PlayAnimation("Hit");
-
-        yield return new WaitForSeconds(0.933f);
-
-        animancer.PlayAnimation(currentAnimName);
-        canRun = true;
-        canSwerve = true;
-    }
-
     public void CanSwerve()
     {
         canSwerve = false;
     }
 
-    public void SwitchPathLine()
-    {
-        pathCreator = pathManager.ReturnCurrenntRoad();
-        distance = 0;
-    }
-
     public void StopMovement()
     {
         //canFollow = false;
-        canRun = false;
-        canSwerve = false;
-    }
-
-    public void ResetCharacter()
-    {
-        PlayAnimation("StartIdle");
-        distance = 0;
-        localMoverTarget.localPosition = new Vector3 (0, 0, 1f);
-        canRun = false;
         canSwerve = false;
     }
 }
